1b3890be12071157d85a060a84c621b698ebe807
haroun-ab <hrnabd@hotmail.com>
2026-02-13 13:47:04 +0100

    Add @ActiveProfiles(test) to all test classes to ensure test profile is activated during CI tests

--- services/discovery/src/test/java/fr/lpmde/discovery/DiscoveryApplicationTests.java
+++ services/discovery/src/test/java/fr/lpmde/discovery/DiscoveryApplicationTests.java
@@ -2,8 +2,10 @@ package fr.lpmde.discovery;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class DiscoveryApplicationTests {
 
 	@Test
