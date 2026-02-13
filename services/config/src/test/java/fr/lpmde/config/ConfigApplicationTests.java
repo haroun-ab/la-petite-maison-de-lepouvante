9ebc69a572213b7e42af9fed7ea7c61e36004e8b
haroun-ab <hrnabd@hotmail.com>
2026-02-13 15:00:05 +0100

    Remove @SpringBootTest from config test to avoid Config Server context loading

--- services/config/src/test/java/fr/lpmde/config/ConfigApplicationTests.java
+++ services/config/src/test/java/fr/lpmde/config/ConfigApplicationTests.java
@@ -1,11 +1,7 @@
 package fr.lpmde.config;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
-@ActiveProfiles("test")
 class ConfigApplicationTests {
 
 	@Test
